cdde3c55bd233c80397ee9c3e98d1d7764409009
ahrezaa <parehold@gmail.com>
2023-05-11 14:43:01 +0700

    UTS Pemrograman WEB

--- application/views/form.blade.php
+++ application/views/form.blade.php
@@ -42,6 +42,11 @@
                 <label for="exampleInputEmail1" class="form-label">Nama</label>
                 <input type="text" class="form-control" id="nama" aria-describedby="nama">
             </div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">NIM</label>
+                <input type="text" class="form-control" id="NIM"> 
+            </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Umur</label>
                 <input type="number" class="form-control" id="umur">
